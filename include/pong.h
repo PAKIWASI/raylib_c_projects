12757f485947187073e286099af9ea8a2ab4897d
PAKIWASI <ullahwasi26@gmail.com>
2026-01-17 23:41:09 +0500

    pong

--- include/pong.h
+++ include/pong.h
@@ -1,19 +1,19 @@
 #pragma once
 
 
-
-#define WIDTH  600.f
-#define HEIGHT 400.f
+#define WIDTH  600.0f
+#define HEIGHT 400.0f
 #define TITLE  "pong"
 
 
-#define NUM_TILES_X 120
-#define NUM_TILES_Y 80
+#define PADDLE_WIDTH  10.0f
+#define PADDLE_HEIGHT 100.0f
+#define PADDLE_SPEED  10.0f
+#define PADDLE_OFFSET 10.0f
 
 
-#define TILE_W (WIDTH / NUM_TILES_X)
-#define TILE_H (HEIGHT / NUM_TILES_Y)
-
+#define BALL_SIZE  10.0f
+#define BALL_SPEED 100.0f
 
 
 void pong_run(void);
--- src/pong.c
+++ src/pong.c
@@ -2,64 +2,139 @@
 #include "raylib.h"
 
 
-#define PADDLE_SIZE_W ((float)(1 * TILE_W))
-#define PADDLE_SIZE_H ((float)(10 * TILE_H))
-
 
 typedef struct {
-    float x;
-    float y;
-    float vx;
-    float vy;
+    Rectangle rect;
+    float     vx;
+    float     vy;
 } Ball;
-
 
 
 void pong_init(Ball* b, Rectangle* lpaddle, Rectangle* rpaddle)
 {
-    InitWindow(WIDTH, HEIGHT, TITLE);
-
+    InitWindow((int)WIDTH, (int)HEIGHT, TITLE);
     SetTargetFPS(60);
 
+    // init the ball (centered)
+    b->rect.x      = (WIDTH / 2.0f) - (BALL_SIZE / 2.0f);
+    b->rect.y      = (HEIGHT / 2.0f) - (BALL_SIZE / 2.0f);
+    b->rect.width  = BALL_SIZE;
+    b->rect.height = BALL_SIZE;
+    b->vx          = BALL_SPEED;
+    b->vy          = BALL_SPEED;
 
-    // init the ball
-    b->x = WIDTH / 2;
-    b->y = HEIGHT / 2;
-    b->vx = 1;
-    b->vy = 1;
+    // init left paddle
+    lpaddle->x      = PADDLE_OFFSET;
+    lpaddle->y      = (HEIGHT / 2.0f) - (PADDLE_HEIGHT / 2.0f);
+    lpaddle->width  = PADDLE_WIDTH;
+    lpaddle->height = PADDLE_HEIGHT;
 
-    // init the paddles
-    lpaddle->x = 0;
-    lpaddle->y = (float) NUM_TILES_Y / 2;
-    lpaddle->width = PADDLE_SIZE_W;
-    lpaddle->height = PADDLE_SIZE_H;
-
-    rpaddle->x = NUM_TILES_X;
-    rpaddle->y = (float) NUM_TILES_Y / 2;
-    rpaddle->width = PADDLE_SIZE_W;
-    rpaddle->height = PADDLE_SIZE_H;
+    // init right paddle
+    rpaddle->x      = WIDTH - PADDLE_OFFSET - PADDLE_WIDTH;
+    rpaddle->y      = (HEIGHT / 2.0f) - (PADDLE_HEIGHT / 2.0f);
+    rpaddle->width  = PADDLE_WIDTH;
+    rpaddle->height = PADDLE_HEIGHT;
 }
 
 
-
-void ball_update(Ball* b, float x, float y)
+void ball_update(Ball* b)
 {
-    b->x += x;
-    b->y += y;
+    float dt = GetFrameTime();
+
+    b->rect.x += b->vx * dt;
+    b->rect.y += b->vy * dt;
+
+    // bounce off top and bottom walls
+    if (b->rect.y <= 0.0f || b->rect.y + b->rect.height >= HEIGHT) { b->vy *= -1.0f; }
+
+    // reset ball if it goes off left or right edge
+    if (b->rect.x + b->rect.width < 0 || b->rect.x > WIDTH) {
+        b->rect.x = (WIDTH / 2.0f) - (BALL_SIZE / 2.0f);
+        b->rect.y = (HEIGHT / 2.0f) - (BALL_SIZE / 2.0f);
+        b->vx     = BALL_SPEED * (b->vx > 0 ? -1.0f : 1.0f); // reverse direction
+    }
 }
 
-// void paddle_update(Rectangle* p, float y)
-// {
-//     p->y += y;
-// }
+
+void paddle_update(Ball* b, Rectangle* paddle)
+{
+    // clamp paddles to screen bounds
+    if (paddle->y < 0.0f) { paddle->y = 0.0f; }
+    if (paddle->y + paddle->height > HEIGHT) { paddle->y = HEIGHT - paddle->height; }
+
+    // handle ball collision with paddles
+    if (CheckCollisionRecs(b->rect, *paddle)) {
+        if (b->vx < 0) {
+            // hit left paddle
+            b->vx     = BALL_SPEED;
+            b->rect.x = paddle->x + paddle->width;
+        } else {
+            // hit right paddle
+            b->vx     = -BALL_SPEED;
+            b->rect.x = paddle->x - b->rect.width;
+        }
+    }
+}
+
 
 void pong_update(Ball* b, Rectangle* lpaddle, Rectangle* rpaddle)
 {
-    // if A is being pressed
-    if (IsKeyDown(KEY_W)) { lpaddle->y++; }
+    // left paddle controls (W/S)
+    if (IsKeyDown(KEY_W)) { lpaddle->y -= PADDLE_SPEED; }
+    if (IsKeyDown(KEY_S)) { lpaddle->y += PADDLE_SPEED; }
 
+    // right paddle controls (I/K)
+    if (IsKeyDown(KEY_I)) { rpaddle->y -= PADDLE_SPEED; }
+    if (IsKeyDown(KEY_K)) { rpaddle->y += PADDLE_SPEED; }
+
+    // update paddles
+    paddle_update(b, lpaddle);
+    paddle_update(b, rpaddle);
+
+    // update ball position
+    ball_update(b);
 }
 
 
+void pong_draw(Ball* b, Rectangle* lpaddle, Rectangle* rpaddle)
+{
+    BeginDrawing();
+    ClearBackground(BLACK);
+
+    // draw paddles
+    DrawRectangleRec(*lpaddle, WHITE);
+    DrawRectangleRec(*rpaddle, WHITE);
+
+    // draw ball
+    DrawRectangleRec(b->rect, WHITE);
+
+    // draw centre line
+    DrawLine((int)WIDTH / 2, 0, (int)WIDTH / 2, HEIGHT, WHITE);
+
+    EndDrawing();
+}
 
 
+void pong_end(void)
+{
+    CloseWindow();
+}
+
+
+void pong_run(void)
+{
+    Ball      b;
+    Rectangle lpaddle;
+    Rectangle rpaddle;
+
+    pong_init(&b, &lpaddle, &rpaddle);
+
+    while (!WindowShouldClose()) {
+
+        pong_update(&b, &lpaddle, &rpaddle);
+
+        pong_draw(&b, &lpaddle, &rpaddle);
+    }
+
+    pong_end();
+}
