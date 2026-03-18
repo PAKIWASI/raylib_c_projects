77e55d323bdba279643d3c84e4f4bc7203295113
PAKIWASI <ullahwasi26@gmail.com>
2026-03-18 16:49:01 +0500

    switch to cpp

--- include/entity.h
+++
@@ -1,8 +0,0 @@
-#ifndef ENTITY_H
-#define ENTITY_H
-
-typedef struct {
-    
-} Entity;
-
-#endif // ENTITY_H
---
+++ src/ray_main.cpp
@@ -0,0 +1,8 @@
+#include <print>
+
+auto main() -> int
+{
+    std::println("hello world");
+
+    return 0;
+}
