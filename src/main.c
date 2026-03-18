2280417040eff728b2067a4b4d298020fff551ea
PAKIWASI <ullahwasi26@gmail.com>
2026-03-18 13:57:24 +0500

    new shi

--- src/main.c
+++ src/main.c
@@ -1,5 +1,17 @@
+#include <raylib.h>
 
+#define WIDTH  600
+#define HEIGHT 480
 
-int main(void) 
-{
+int main(void)
+{ 
+    InitWindow(WIDTH, HEIGHT, "g");
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground((Color){10, 100, 10, 1});
+        EndDrawing();
+    }
+
+    CloseWindow();
 }
